f1658340c181fdb8be95deda15ec0108906bdf3a
DvijIt <andreydnb@ukr.net>
2022-02-06 13:58:23 +0200

    add API dosc swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function start() {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+
+    const config = await new DocumentBuilder()
+      .setTitle('shop-app')
+      .setDescription('REST API Documentation')
+      .setVersion('1.0.0')
+      .addTag('DVIJIT')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('/api/docs', app, document);
+
     await app.listen(PORT, () =>
       console.log(`Server started on port = ${PORT}`),
     );
--- server/src/users/users.model.ts
+++ server/src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 interface UserCreationAttributes {
@@ -7,6 +8,7 @@ interface UserCreationAttributes {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttributes> {
+  @ApiProperty({ example: '1', description: 'Уникальный идентификатор' })
   @Column({
     type: DataType.INTEGER,
     unique: true,
@@ -14,16 +16,18 @@ export class User extends Model<User, UserCreationAttributes> {
     primaryKey: true,
   })
   id: number;
-
+  @ApiProperty({ example: 'user@mail.ru', description: 'Почтовый адрес' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
-
+  @ApiProperty({ example: '12345678', description: 'Пароль' })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
 
+  @ApiProperty({ example: 'true', description: 'Забанен или нет' })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
 
+  @ApiProperty({ example: 'За хулиганство', description: 'Причина блокировки' })
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string;
 }
