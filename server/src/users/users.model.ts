d9c781462fa3383ea31d0372b0ec2d5f2786fbdc
DvijIt <andreydnb@ukr.net>
2022-02-06 14:27:36 +0200

    add UserRoles model

--- server/src/users/users.model.ts
+++ server/src/users/users.model.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { Role } from 'src/roles/roles.model';
+import { UserRoles } from 'src/roles/user-roles.model';
 
 interface UserCreationAttributes {
   email: string;
@@ -16,6 +24,7 @@ export class User extends Model<User, UserCreationAttributes> {
     primaryKey: true,
   })
   id: number;
+
   @ApiProperty({ example: 'user@mail.ru', description: 'Почтовый адрес' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
@@ -30,4 +39,7 @@ export class User extends Model<User, UserCreationAttributes> {
   @ApiProperty({ example: 'За хулиганство', description: 'Причина блокировки' })
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
 }
